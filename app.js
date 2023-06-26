cbfebea1be0c82e5edc8d36b8ec4e451d209f82f
unknown <Raphael.zanzouri@gmail.com>
2023-06-26 16:49:26 +0300

    added app configuration to the config files

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const config = require('config');
+
+console.log(config.get('app.host'));
+console.log(config.get('app.port'));
 
 const guestsRouter = require('./routes/guests')
 const usersRouter = require('./routes/users');
@@ -7,6 +11,9 @@ const githubRouter = require('./routes/github');
 
 const pageNotFound = require('./middlewares/404')
 const error = require('./middlewares/error');
+
+
+
 
 const app = express();
 
